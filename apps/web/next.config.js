d1e8ce796af7840c0d238dd6433d3f9b3f02ef13
Gerald Mumo <geraldmumo6@gmail.com>
2026-06-06 19:34:53 +0300

    updates on the next,js config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,15 +4,14 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
 const apiHostname = new URL(API_URL.replace(/\/api$/, '')).hostname;
 
 const nextConfig = {
+    output:'standalone',
   images: {
     remotePatterns: [
-      // OpenWeatherMap fallback icons
       {
         protocol: 'https',
         hostname: 'openweathermap.org',
         pathname: '/img/wn/**',
       },
-      // Your NestJS API icons (e.g. /icons/weather/png/...)
       {
         protocol: 'http',
         hostname: apiHostname,
